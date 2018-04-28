0bb7eadfec30996cc36c7ecd174c5efff5ff4132
Jordan Page <jpage628@gmail.com>
2018-04-28 10:35:53 -0400

    comments

--- gAnswer.py
+++ gAnswer.py
@@ -2,6 +2,9 @@
 
 
 # Takes some string s that looks like "abccbaabccba"
+# returns the number of pieces that can be equally cut.
+# returns 1 if the string can't be totally cut
+# returns 0 if the string is empty or non-alphabetic
 def Answer(s):
     s = s.strip()
 
@@ -13,6 +16,7 @@ def Answer(s):
 
     # str1 will build like this:
     # 'a', 'ab', 'abc', 'abcc', 'abccb', etc
+    # and attempt to fully replace the original string
     for c in s:
         str1 += c
         if s.replace(str1, '') == '':
